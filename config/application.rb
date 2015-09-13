aa98468d208b286b8bb3e3482598c2adf2ce6f8d
Orion Osborn <orion.osborn@gmail.com>
2015-09-13 17:52:05 -0600

    create seeds file for easy loading in the future

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,6 @@ module TuringBlogs
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.dependency_loading = true if $rails_rake_task
   end
 end
